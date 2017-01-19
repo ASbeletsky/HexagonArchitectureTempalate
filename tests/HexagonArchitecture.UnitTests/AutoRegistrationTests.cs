9cf7d8cf19810083a3195cc70ca5eee8e262bad2
DEV\beletsky.anton <antonbeletsky@gmail.com>
2017-01-19 20:05:12 +0200

    remove autoregistration

--- tests/HexagonArchitecture.UnitTests/AutoRegistrationTests.cs
+++ tests/HexagonArchitecture.UnitTests/AutoRegistrationTests.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using Xunit;
-using System.Reflection;
-using HexagonArchitecture.Domain.Interfaces.Cqrs;
-using HexagonArchitecture.Infrastructure.Components;
-using HexagonArchitecture.Mocks;
-using HexagonArchitecture.Services.Common.Sqrs.GenericCommands;
-using HexagonArchitecture.Services.Common.Sqrs.GenericQueries;
+﻿using Xunit;
 
 namespace HexagonArchitecture.UnitTests
 {
@@ -14,16 +7,17 @@ namespace HexagonArchitecture.UnitTests
         [Fact]
         public void AutoRegistration_MainComponentsSetUp()
         {
-            var assembly = this.GetType().GetTypeInfo().Assembly;
-            var res = new AutoRegistration().GetComponentMap(assembly, t => t == typeof(TestDependentObject), assembly, t => true);
+/*            var currentAssembly = this.GetType().GetTypeInfo().Assembly;
+            var mocksAssembly = typeof(TestDependentObject).GetTypeInfo().Assembly;
+            var res = new AutoRegistration().GetComponentMap(mocksAssembly, t => t == typeof(TestDependentObject), mocksAssembly, t => true);
 
             Assert.Equal(
                 typeof(ProjectionQuery<object, SomeEntity, SomeEntityDto>),
                 res[typeof(IQuery<object, IEnumerable<SomeEntityDto>>)]);
 
-            /*Assert.Equal(
+            Assert.Equal(
                 typeof(PagedQuery<int, IdPaging<SomeEntityDto>, SomeEntity, SomeEntityDto>),
-                res[typeof(IQuery<IdPaging<SomeEntityDto>, IPagedEnumerable<SomeEntityDto>>)]);*/
+                res[typeof(IQuery<IdPaging<SomeEntityDto>, IPagedEnumerable<SomeEntityDto>>)]);
 
             Assert.Equal(
                 typeof(GetByIdQuery<int, SomeEntity, SomeEntityDto>),
@@ -31,6 +25,7 @@ namespace HexagonArchitecture.UnitTests
 
             Assert.Equal(
                 typeof(CreateOrUpdateHandler<int, SomeEntityDto, SomeEntity>),
-                res[typeof(ICommandHandler<SomeEntityDto, int>)]);
+                res[typeof(ICommandHandler<SomeEntityDto, int>)]);*/
         }
+    }
 }
